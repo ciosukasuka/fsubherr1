5716eb04d9e2de7a626c32538b9d0835f9605797
ciosukasuka <104566471+ciosukasuka@users.noreply.github.com>
2024-06-03 23:32:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -18,11 +18,11 @@ class Config:
 
     API_ID = int(os.environ.get('API_ID', 27538621))
     API_HASH = os.environ.get('API_HASH', '24c338d23154f5ea8adb9fc684a4a06f')
-    OWNER_ID = int(os.environ.get('OWNER_ID', 487936750))
+    OWNER_ID = int(os.environ.get('OWNER_ID', 1780709155))
     MONGO_URL = os.environ.get('MONGO_URL', 'mongodb://root:passwd@mongo')
 
     BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
-    DATABASE_ID = int(os.environ.get('DATABASE_ID', ''))
+    DATABASE_ID = int(os.environ.get('DATABASE_ID', '-1002068464309'))
 
     BOT_ID = BOT_TOKEN.split(':', 1)[0]
 
